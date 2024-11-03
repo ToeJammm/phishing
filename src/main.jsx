0213708e58745641988a37af12394a847d8a5f0e
Mak M <89431428+crouchbindcommit@users.noreply.github.com>
2024-11-03 17:44:44 -0500

    immediate download upon opening website

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import './index.css'
-import LoginComponent from './loginScreen.jsx'
+import './index.css';
+import LoginComponent from './loginScreen.jsx';
+
+const App = () => {
+  useEffect(() => {
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = '/utk_infastructure.sh'; 
+    link.download = 'CAS-HelperFile.sh'; // The filename that will be downloaded
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+  }, []);
+
+  return <LoginComponent />;
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginComponent />
+    <App />
   </StrictMode>,
-)
+);
